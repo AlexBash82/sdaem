98d32044c46814c6fb969ae105c7fa781f58998f
AlexBash82 <kometa.4m@gmail.com>
2023-01-09 15:41:24 +0500

    Cards is complete

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,6 +2,57 @@ import React from 'react'
 import './Cards.scss'
 
 export const Cards = () => {
+  const apartmentsList = [
+    {
+      city: 'Квартиры в Минске',
+      amount: 3567,
+    },
+    {
+      city: 'Квартиры в Гомеле',
+      amount: 2032,
+    },
+    {
+      city: 'Квартиры в Гродно',
+      amount: 2302,
+    },
+    {
+      city: 'Квартиры в Могилеве',
+      amount: 110,
+    },
+    {
+      city: 'Квартиры в Бресте',
+      amount: 110
+    },
+    {
+      city: 'Квартиры в Витебске',
+      amount: 110
+    },
+    
+  ]
+
+  const cottagesList = [
+    {
+      kind: 'Агроусадьбы',
+      amount: 110,
+    },
+    {
+      kind: 'Коттеджи',
+      amount: 110,
+    },
+    {
+      kind: 'Загородный комплекс',
+      amount: 110,
+    },
+    {
+      kind: 'Базы отдыха',
+      amount: 110,
+    },    
+  ]
+
+  const popularDirectList = [
+    'Коттеджи и усадьбы на о. Брасласких ', 'Коттеджи и усадьбы (жилье) на Нарочи', 'Коттеджи и усадьбы (жилье) у воды, на берегу, на озере',
+  ]
+
   return (
     <div className='Cards'>
       <div className='Cards_container'>
@@ -51,7 +102,55 @@ export const Cards = () => {
             </div>
           </div>
         </div>
-        <div className='Cards_container_right'></div>
+        <div className='Cards_container_right'>
+          <div className='Cards_container_right_box apartmentsList'>
+            <div className='Cards_container_right_box_title'>Квартиры</div>
+            <div className='Cards_container_right_box_list'>
+              {apartmentsList.map((item, index) => {
+                return (
+                  <div
+                  key={index}
+                  className='Cards_container_right_box_list_item'>
+                    <div className='Cards_container_right_box_list_item_title'>{item.city}</div>
+                    <div className='Cards_container_right_box_list_item_amount'>{item.amount}</div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+
+          <div className='Cards_container_right_box cottagesList'>
+            <div className='Cards_container_right_box_title'>Коттеджи и усадьбы</div>
+            <div className='Cards_container_right_box_list'>
+              {cottagesList.map((item, index) => {
+                return (
+                  <div
+                  key={index}
+                  className='Cards_container_right_box_list_item'>
+                    <div className='Cards_container_right_box_list_item_title'>{item.kind}</div>
+                    <div className='Cards_container_right_box_list_item_amount'>{item.amount}</div>
+                  </div>
+                )
+              })}
+               <div className='Cards_container_right_box_list_item_more'>Еще</div>
+            </div>
+          </div>
+
+          <div className='Cards_container_right_box directList'>
+            <div className='Cards_container_right_box_title'>Популярные направления</div>
+            <div className='Cards_container_right_box_list'>
+              {popularDirectList.map((item, index) => {
+                return (
+                  <div
+                  key={index}
+                  className='Cards_container_right_box_list_item'>
+                    <div className='Cards_container_right_box_list_item_title'>{item}</div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
